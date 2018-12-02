f3e487ce715c3aca9427f3bfb3a2f1d4f8c656a7
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-12-01 21:30:08 -0500

    Basic Configuration

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from '../Header/Header';
+import Main1 from '../Main1/Main1';
+import Main2 from '../Main2/Main2';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" id ="app">
+        <Header/>
+        <Main1/>
+        <Main2/>
+        <Main3/>
+      </div>
+    );
+  }
+}
+
+export default App;
