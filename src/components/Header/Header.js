51ddd36ab628ee11549189089d72bbf18dacfec3
Vasilis Georgoudis <masakaftw@hotmail.com>
2019-01-21 17:07:28 -0500

    Header complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import './Header.css';
+import logo from './2071-logo.jpg';
 
 class Header extends Component {
   render() {
     return (
       <header className="header">
-        Header
+        <img src={logo} alt="logo" className="header__logo"/>
+          <ul className="header__navigation">
+            <li>About</li>
+            <li>Products & Offering</li>
+            <li>Space & Design</li>
+            <li>Challenge</li>
+            <li>Join Us</li>
+          </ul>
       </header>
     );
   }
