ba6f5ddad95a1c5baf487a3f3c2dc4c9742fefd8
Vasilis Georgoudis <masakaftw@hotmail.com>
2019-01-24 13:06:46 -0500

    Image Color Adjustments

--- src/components/Main1picture/Main1picture.js
+++ src/components/Main1picture/Main1picture.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Main1picture.css';
-import imageSmall from './image-small.jpg';
-import imageBig from './image-big.jpg';
+import imageSmall from './image-small.png';
+import imageBig from './image-big.png';
 
 class Main1picture extends Component {
   render() {
