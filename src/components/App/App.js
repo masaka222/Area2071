aadbf3e0739af863b159b78fabeaad2a3ffb3c94
Vasilis Georgoudis <masakaftw@hotmail.com>
2019-01-26 11:20:38 -0500

    React Scroll Parallax installment

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,6 +7,15 @@ import Main3 from '../Main3/Main3';
 
 class App extends Component {
   render() {
+
+    window.addEventListener('scroll', function (e) {
+      // will not work on IE < 9
+      var scrolled = window.pageYOffset;
+      // will not work on IE < 8
+      const background = document.querySelector('.main1');
+      background.style.top = - (scrolled * 0.2) + 'px';
+    });
+
     return (
       <div className="App" id ="app">
         <Header/>
