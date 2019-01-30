a4c8714cce25b1a3eb5bb925d68af420f1836185
Vasilis Georgoudis <masakaftw@hotmail.com>
2019-01-30 15:46:46 -0500

    Main2 Basic text

--- src/components/Main2content/Main2content.js
+++ src/components/Main2content/Main2content.js
@@ -6,7 +6,22 @@ class Main2content extends Component {
   render() {
     return (
       <div className="main2content">
-        main2content
+        <div id = "main2content">
+          <h2 className="main2content__title">We will unlock people's potential to imagine and </h2>
+          <h2 className="main2content__title">create new solutions to our greatest challenges</h2>
+
+          <div className="main2content__text">
+              <p>The UAE has a vision for the future based on radical creativity
+              curiosity and generosity and invite the world's most curious minds and creative institutions to 
+              co-create this future with us
+              </p>
+              <u>Creative thinkers from government, business, and culture will design the future together</u>
+              <p>AREA 2071 is a creative ecosystem to design the future that includes government, creative
+                individuals, business, and people from all walks from life. We've come together to imagine
+                and create solutions to the world's most pressing questions
+              </p>
+          </div>
+        </div>
       </div>
     );
   }
