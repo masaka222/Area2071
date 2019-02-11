a08a847b5bd7cd6a5759634df86349fccf27f26a
Vasilis Georgoudis <masakaftw@hotmail.com>
2019-02-11 10:38:39 -0500

    Quote component complete

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './Quote.css';
+import chevrons from './chevrons.png';
 
 class Quote extends Component {
   render() {
     return (
       <div className="quote">
-        Quote
+        <img src={chevrons} alt="chevrons" className="quote__logo"/>
+        <p className="quote__maintext1"> 
+            "We are building a new reality for our people, a new future
+        </p>
+        <p className="quote__maintext1">
+            for our children, and a new model of development."
+        </p>
+        <p className="quote__subtext">
+            - Sheikh Mohammed bin Rashid al Maktoum
+        </p>
       </div>
     );
   }
