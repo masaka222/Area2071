4711619c1ed7c7aa0daf4de074925302276ee88e
Vasilis Georgoudis <masakaftw@hotmail.com>
2019-02-16 15:06:03 -0500

    Leaves in Main1 - added

--- src/components/Main1picture/Main1picture.js
+++ src/components/Main1picture/Main1picture.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './Main1picture.css';
 import imageSmall from './image-small.png';
 import imageBig from './image-big.png';
+import orangeLeaf from './orange-leaf.png'
+import redLeaf from './red-leaf.png'
 
 class Main1picture extends Component {
   render() {
@@ -9,6 +11,8 @@ class Main1picture extends Component {
       <div className="main1picture">
         <img src={imageBig} alt="imageBig" id = "imageBig" className="main1picture-big"/>
         <img src={imageSmall} alt="imageSmall" id="imageSmall" className="main1picture-small"/>
+        <img src={orangeLeaf} alt="orangeLeaf" id="orangeLeaf" className="main1picture-orangeLeaf"/>
+        <img src={redLeaf} alt="redLeaf" id="redLeaf" className="main1picture-redLeaf"/>
       </div>
     );
   }
