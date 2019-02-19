6c67b7a0a4252b8e266fcf023e805d628d8933af
Vasilis Georgoudis <masakaftw@hotmail.com>
2019-02-19 01:27:56 -0500

    Updates

---
+++ precache-manifest.d87407608fe3508d9f78a784dbbf7ea6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "d075511c3ac31aea242d929e67dda949",
+    "url": "/area2071/static/media/image-big.d075511c.png"
+  },
+  {
+    "revision": "bbe9ce3e937e987f0c53",
+    "url": "/area2071/static/css/main.6dcba965.chunk.css"
+  },
+  {
+    "revision": "5e7e23c24ff14850e220",
+    "url": "/area2071/static/js/runtime~main.5e7e23c2.js"
+  },
+  {
+    "revision": "5bbc0f97ee69ec7644f8",
+    "url": "/area2071/static/js/2.5bbc0f97.chunk.js"
+  },
+  {
+    "revision": "b5fa9a263b1ddd8bfb3b7f670a8de6f6",
+    "url": "/area2071/static/media/2071-logo.b5fa9a26.jpg"
+  },
+  {
+    "revision": "b743f78d12453da57e9564969f2e26fd",
+    "url": "/area2071/static/media/image-small.b743f78d.png"
+  },
+  {
+    "revision": "bbe9ce3e937e987f0c53",
+    "url": "/area2071/static/js/main.bbe9ce3e.chunk.js"
+  },
+  {
+    "revision": "ac9c307d7c1148166d50e8c063bbd972",
+    "url": "/area2071/static/media/orange-leaf.ac9c307d.png"
+  },
+  {
+    "revision": "84477ae43bac942edd41e1a54743d062",
+    "url": "/area2071/static/media/red-leaf.84477ae4.png"
+  },
+  {
+    "revision": "1480a8ab751c6c61dfe317c77da7a643",
+    "url": "/area2071/static/media/vision-eye.1480a8ab.png"
+  },
+  {
+    "revision": "89c345c7cec16b15ca25d883ccdd26c3",
+    "url": "/area2071/static/media/lounge.89c345c7.jpg"
+  },
+  {
+    "revision": "0a21dd67b9f1aef4700368e8fe85af0a",
+    "url": "/area2071/static/media/topo.0a21dd67.png"
+  },
+  {
+    "revision": "25aceb6771e6271fa610d3cca69de60f",
+    "url": "/area2071/index.html"
+  }
+];
